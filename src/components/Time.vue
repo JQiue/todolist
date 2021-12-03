c5c11006ae91856f5ee50808464b831d24d2cdb2
JQiue <861947542@qq.com>
2021-12-03 21:22:34 +0800

    fix: 修复日期错误问题

--- src/components/Time.vue
+++ src/components/Time.vue
@@ -34,7 +34,7 @@ export default {
   filters: {
     dayFormat(value){
       const days = ['星期一', '星期二','星期三','星期四','星期五','星期六','星期日'];
-      return days[value];
+      return days[value - 1];
     },
   },
   created(){
